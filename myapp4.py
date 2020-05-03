ffed38dbd742e050fef0e604e21ef139c44517c4
Roberto <Roonyi@gmail.com>
2020-05-02 20:47:40 -0500

    first update

---
+++ myapp4.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+print("hello world")
+print('hola mundo')
+print('hello world')
+print('hola mundo')
+print('hello world')
+print('hola mundo')
+print('hello world')
+X=0
+print(X)
+print('hello world')
+print('hola mundo')
